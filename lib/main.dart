def509a053ab0255e6a416ccc9bdaa5b78e4e97b
Carlos <cpinelly@gmail.com>
2020-07-04 10:12:24 -0500

    Project start

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Savings App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Savings App"),
+        ),
+        body: Center(
+          child: Text("Your App Dashboard"),
+        ),
+      ),
+    );
+  }
+}
