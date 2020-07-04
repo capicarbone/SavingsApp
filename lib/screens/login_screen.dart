eba4d83e9b2c5d64ef1ef5c780b1955f198dd60b
Carlos <cpinelly@gmail.com>
2020-07-04 18:38:35 -0500

    Basic Login form validation.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:developer' as dev;
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -6,34 +7,78 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  var _formData = {
+    'email': '',
+    'password': ''
+  };
+
+  void _submit() {
+    if (_formKey.currentState.validate()) {
+      dev.log("Valid Form");
+      _formKey.currentState.save();
+    }
+  }
+
+  String _validateEmail(String value){
+    if (value.isEmpty){
+      return "Email is empty";
+    }
+
+    if (!value.contains("@")  || !value.contains(".")){
+      return "Not a valid email";
+    }
+  }
+
+  String _validatePassword(String value){
+    if (value.isEmpty || value.length < 5) {
+      return 'Password is too short!';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login"),),
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
       body: Container(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20,),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'E-mail'),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              RaisedButton(
-                child: Text("Sign in"),
-                color: Theme.of(context).primaryColor,
-                textColor: Theme.of(context).primaryTextTheme.button.color,
-                onPressed: () {},
-              )
-            ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'E-mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: _validateEmail ,
+                  onSaved: (value) {
+                    _formData['email'] = value;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  validator: _validatePassword,
+                  onSaved: (value) {
+                    _formData['password'] = value;
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  child: Text("Sign in"),
+                  color: Theme.of(context).primaryColor,
+                  textColor: Theme.of(context).primaryTextTheme.button.color,
+                  onPressed: _submit,
+                )
+              ],
+            ),
           ),
         ),
       ),
