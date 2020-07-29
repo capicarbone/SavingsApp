7409c740098ffb2a26d19bc6d623d156a51c4b0e
Carlos <cpinelly@gmail.com>
2020-07-29 09:58:15 -0500

    Category model.

---
+++ lib/models/category.dart
@@ -0,0 +1,13 @@
+
+import 'package:meta/meta.dart';
+
+class Category {
+  String id;
+  String name;
+
+  Category({@required this.id, @required this.name});
+
+  factory Category.fromMap(Map<String, dynamic> map) {
+    return Category(name: map['name'], id: map['id']);
+  }
+}
