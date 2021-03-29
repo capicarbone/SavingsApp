7b83c232a4c01be91c62817d47314b75f7fb2fc4
Carlos Pinelly <cpinelly@gmail.com>
2021-03-28 19:19:36 -0500

    Fetch method for month statements repository.

---
+++ lib/repositories/month_statements_repository.dart
@@ -0,0 +1,37 @@
+import 'dart:collection';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:savings_app/models/month_statement.dart';
+import 'package:savings_app/repositories/web_repository.dart';
+
+class MonthStatementsRepository extends WebRepository {
+  MonthStatementsRepository({String authToken}) : super(authToken: authToken);
+
+  static const resourceUri = "reports/month_statements";
+
+  String get _resourceUrl => getHost() + resourceUri;
+
+  // TODO: add month and year as parameters
+  Future<List<MonthStatement>> fetch([int page = 0]) async {
+
+    var url = "$_resourceUrl?page=$page";
+
+    final response =
+        await http.get(url, headers: getAuthenticatedHeader(), );
+
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      LinkedHashMap<String, dynamic> parsedResponse =
+          json.decode(response.body);
+
+      List<MonthStatement> statements = (parsedResponse['_items'] as List<dynamic>)
+          .map((e) => MonthStatement.fromMap(e))
+          .toList();
+
+      return statements;
+    } else {
+      throw Exception(response.body);
+    }
+  }
+}
