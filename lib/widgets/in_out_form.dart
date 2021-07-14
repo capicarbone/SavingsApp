f30d05d5025c8a64278641d5aa0b7b05a96170d1
capi <cpinelly@gmail.com>
2021-07-13 23:11:20 -0500

    Unfocus on form fields when transaction registered.

--- lib/widgets/in_out_form.dart
+++ lib/widgets/in_out_form.dart
@@ -17,7 +17,6 @@ import 'package:dropdown_search/dropdown_search.dart';
 
 import '../blocs/in_out_form/in_out_form_states.dart';
 
-
 class InOutForm extends StatefulWidget {
   final bool expenseMode;
 
@@ -30,6 +29,8 @@ class InOutForm extends StatefulWidget {
 class _InOutFormState extends State<InOutForm> {
   final _formKey = GlobalKey<FormState>();
   InOutFormBloc _bloc;
+  FocusNode _descriptionNode;
+  FocusNode _amountNode;
 
   final amountController = TextEditingController();
   final descriptionController = TextEditingController();
@@ -39,6 +40,9 @@ class _InOutFormState extends State<InOutForm> {
   DateTime _selectedDate = DateTime.now();
 
   void _cleanForm() {
+    _descriptionNode.unfocus();
+    _amountNode.unfocus();
+
     setState(() {
       _selectedDate = DateTime.now();
       amountController.text = "";
@@ -50,8 +54,6 @@ class _InOutFormState extends State<InOutForm> {
 
   void _submitForm(ctx) {
     var bloc = BlocProvider.of<InOutFormBloc>(ctx);
-
-    // TODO: Validate form
 
     var event = InOutFormSubmitEvent(
         amount: amountController.text,
@@ -69,9 +71,21 @@ class _InOutFormState extends State<InOutForm> {
 
     // TODO: Create respositories in the InOutFormBloc
 
+    _descriptionNode = FocusNode();
+    _amountNode = FocusNode();
+
     _bloc = InOutFormBloc(
       expenseMode: widget.expenseMode,
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _descriptionNode.dispose();
+    _amountNode.dispose();
+    amountController.dispose();
+    descriptionController.dispose();
   }
 
   @override
@@ -81,151 +95,165 @@ class _InOutFormState extends State<InOutForm> {
         return _bloc;
       },
       child: BlocConsumer<InOutFormBloc, InOutFormState>(
+          listenWhen: (_, current) =>
+              current is InOutFormSubmitFailedState ||
+              current is InOutFormSubmittedState,
           listener: (context, state) {
-        if (state is InOutFormSubmitFailedState) {
-          Scaffold.of(context).showSnackBar(SnackBar(
-            content: Text('An error has ocurred.'),
-            backgroundColor: Colors.red,
-          ));
-        }
 
-        if (state is InOutFormSubmittedState) {
-          _cleanForm();
-          Scaffold.of(context).showSnackBar(SnackBar(
-            content: Text("Transaction Saved"),
-            backgroundColor: Colors.green,
-          ));
+            if (state is InOutFormSubmitFailedState) {
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text('An error has ocurred.'),
+                backgroundColor: Colors.red,
+              ));
 
-          var syncerBloc = BlocProvider.of<SettingsSyncerBloc>(context);
-          syncerBloc.add(ReloadLocalData(accounts: true, funds: true));
-        }
-      }, builder: (ctx, state) {
-        return Form(
-          key: _formKey,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                controller: amountController,
-                keyboardType: TextInputType.number,
-                autovalidate: true,
-                validator: (v) {
-                  if (state is InOutFormInvalidState && state.hasAmountError) {
-                    return state.amountErrorMessage;
-                  }
+            }
 
-                  return null;
-                },
-                decoration: const InputDecoration(
-                  hintText: "Amount",
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    child: Text(
-                      DateFormat.yMd().format(_selectedDate),
-                    ),
-                  ),
-                  RaisedButton(
-                    child: Text("Pick a date"),
-                    onPressed: () {
-                      var now = DateTime.now();
-                      showDatePicker(
-                              context: ctx,
-                              firstDate: DateTime(now.year - 1, 1, 1),
-                              initialDate: _selectedDate,
-                              lastDate: DateTime(now.year + 1, 1, 1))
-                          .then((value) {
-                        setState(() {
-                          _selectedDate = value != null ? value : _selectedDate;
-                        });
-                      });
-                    },
-                  ),
-                ],
-              ),
-              BlocBuilder<SettingsSyncerBloc, SettingsSyncState>(
-                  buildWhen: (_, dataState) => dataState is DataContainerState,
-                  builder: (context, dataState) {
-                    var data = dataState as DataContainerState;
-                    return DropdownButtonFormField(
-                      onChanged: (accountId) {
-                        _selectedAccount = accountId;
-                      },
-                      value: _selectedAccount,
-                      autovalidate: true,
-                      validator: (value) {
-                        if (state is InOutFormInvalidState &&
-                            state.hasAccountError) {
-                          return state.accountErrorMessage;
-                        }
+            if (state is InOutFormSubmittedState) {
+              _cleanForm();
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text("Transaction Saved"),
+                backgroundColor: Colors.green,
+              ));
 
-                        return null;
-                      },
-                      decoration: const InputDecoration(hintText: "Account"),
-                      items: [
-                        ...data.accounts.map((e) => DropdownMenuItem(
-                              child: Text(e.name),
-                              value: e.id,
-                            ))
-                      ],
-                    );
-                  }),
-              SizedBox(height: 24),
-              BlocBuilder<SettingsSyncerBloc, SettingsSyncState>(
-                  buildWhen: (_, dataState) =>
-                      dataState is DataContainerState &&
-                      dataState.categories != null,
-                  builder: (context, dataState) {
-                    var data = dataState as DataContainerState;
-
-                    var categoriesFiltered = data.categories.where((category) {
-                      if (widget.expenseMode) {
-                        return !category.isIncome;
+              var syncerBloc = BlocProvider.of<SettingsSyncerBloc>(context);
+              syncerBloc.add(ReloadLocalData(accounts: true, funds: true));
+            }
+          },
+          builder: (ctx, state) {
+            return Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    focusNode: _amountNode,
+                    controller: amountController,
+                    keyboardType: TextInputType.number,
+                    autovalidate: true,
+                    validator: (v) {
+                      if (state is InOutFormInvalidState &&
+                          state.hasAmountError) {
+                        return state.amountErrorMessage;
                       }
 
-                      return category.isIncome;
-                    }).toList();
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      hintText: "Amount",
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: Text(
+                          DateFormat.yMd().format(_selectedDate),
+                        ),
+                      ),
+                      RaisedButton(
+                        child: Text("Pick a date"),
+                        onPressed: () {
+                          var now = DateTime.now();
+                          showDatePicker(
+                                  context: ctx,
+                                  firstDate: DateTime(now.year - 1, 1, 1),
+                                  initialDate: _selectedDate,
+                                  lastDate: DateTime(now.year + 1, 1, 1))
+                              .then((value) {
+                            setState(() {
+                              _selectedDate =
+                                  value != null ? value : _selectedDate;
+                            });
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  BlocBuilder<SettingsSyncerBloc, SettingsSyncState>(
+                      buildWhen: (_, dataState) =>
+                          dataState is DataContainerState,
+                      builder: (context, dataState) {
+                        var data = dataState as DataContainerState;
+                        return DropdownButtonFormField(
+                          onChanged: (accountId) {
+                            _selectedAccount = accountId;
+                          },
+                          value: _selectedAccount,
+                          autovalidate: true,
+                          validator: (value) {
+                            if (state is InOutFormInvalidState &&
+                                state.hasAccountError) {
+                              return state.accountErrorMessage;
+                            }
 
-                    categoriesFiltered.sort((categoryA, categoryB) =>
-                        categoryA.name.compareTo(categoryB.name));
+                            return null;
+                          },
+                          decoration:
+                              const InputDecoration(hintText: "Account"),
+                          items: [
+                            ...data.accounts.map((e) => DropdownMenuItem(
+                                  child: Text(e.name),
+                                  value: e.id,
+                                ))
+                          ],
+                        );
+                      }),
+                  SizedBox(height: 24),
+                  BlocBuilder<SettingsSyncerBloc, SettingsSyncState>(
+                      buildWhen: (_, dataState) =>
+                          dataState is DataContainerState &&
+                          dataState.categories != null,
+                      builder: (context, dataState) {
+                        var data = dataState as DataContainerState;
 
-                    return DropdownSearch<Category>(
-                      label: "Category",
-                      items: categoriesFiltered,
-                      selectedItem: _selectedCategory,
-                      autoValidateMode: AutovalidateMode.onUserInteraction,
-                      validator: (Category c) => c == null ? "Required" : null,
-                      showSearchBox: true,
-                      popupItemBuilder: (BuildContext context, Category item,
-                          bool isSelected) {
-                        return _categoryItemBuilder(
-                            context, item, data.funds, isSelected);
-                      },
-                      onChanged: (category) {
-                        _selectedCategory = category;
-                      },
-                    );
-                  }),
-              TextFormField(
-                controller: descriptionController,
-                decoration: const InputDecoration(hintText: "Description"),
+                        var categoriesFiltered =
+                            data.categories.where((category) {
+                          if (widget.expenseMode) {
+                            return !category.isIncome;
+                          }
+
+                          return category.isIncome;
+                        }).toList();
+
+                        categoriesFiltered.sort((categoryA, categoryB) =>
+                            categoryA.name.compareTo(categoryB.name));
+
+                        return DropdownSearch<Category>(
+                          label: "Category",
+                          items: categoriesFiltered,
+                          selectedItem: _selectedCategory,
+                          autoValidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (Category c) =>
+                              c == null ? "Required" : null,
+                          showSearchBox: true,
+                          popupItemBuilder: (BuildContext context,
+                              Category item, bool isSelected) {
+                            return _categoryItemBuilder(
+                                context, item, data.funds, isSelected);
+                          },
+                          onChanged: (category) {
+                            _selectedCategory = category;
+                          },
+                        );
+                      }),
+                  TextFormField(
+                    controller: descriptionController,
+                    focusNode: _descriptionNode,
+                    decoration: const InputDecoration(hintText: "Description"),
+                  ),
+                  RaisedButton(
+                    child: Text("Save"),
+                    onPressed: (state is InOutFormSubmittingState)
+                        ? null
+                        : () {
+                            _submitForm(ctx);
+                          },
+                  )
+                ],
               ),
-              RaisedButton(
-                child: Text("Save"),
-                onPressed: (state is InOutFormSubmittingState)
-                    ? null
-                    : () {
-                        _submitForm(ctx);
-                      },
-              )
-            ],
-          ),
-        );
-      }),
+            );
+          }),
     );
   }
 
