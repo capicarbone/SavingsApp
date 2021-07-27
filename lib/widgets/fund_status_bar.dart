3a613f578e09c8812152dd7b392b6938c2e3e6d1
capi <cpinelly@gmail.com>
2021-07-27 12:15:47 -0500

    Improvements on fund status bar.

--- lib/widgets/fund_status_bar.dart
+++ lib/widgets/fund_status_bar.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:savings_app/models/fund.dart';
 
@@ -10,22 +9,17 @@ class FundStatusBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
-
-      final width = constraints.maxWidth;
-      final currentWith = width - width*0.25;
-      final height = 20.0;
-
       double currentGoal = 0;
       double previousGoal = 0;
       double nextGoal = 0;
 
       if (fund.minimumLimit != null && fund.maximumLimit != null) {
-        if (balance < fund.minimumLimit){
+        if (balance < fund.minimumLimit) {
           currentGoal = fund.minimumLimit;
           nextGoal = fund.maximumLimit;
         }
 
-        if (balance > fund.maximumLimit && balance < fund.minimumLimit){
+        if (balance > fund.minimumLimit && balance < fund.maximumLimit) {
           currentGoal = fund.maximumLimit;
           previousGoal = fund.minimumLimit;
         }
@@ -34,53 +28,69 @@ class FundStatusBar extends StatelessWidget {
           previousGoal = fund.maximumLimit;
           currentGoal = -1;
         }
-      }else{
-        if (fund.minimumLimit != null && balance < fund.minimumLimit ){
+      } else {
+        if (fund.minimumLimit != null && balance < fund.minimumLimit) {
           currentGoal = fund.minimumLimit;
         }
 
-        if (fund.maximumLimit != null && balance < fund.maximumLimit ) {
+        if (fund.maximumLimit != null && balance < fund.maximumLimit) {
           currentGoal = fund.maximumLimit;
         }
       }
 
-      var currentToCurrentGoal = (currentGoal == -1) ? 1 : balance / currentGoal;
+      var currentToCurrentGoal =
+          (currentGoal == -1) ? 1 : balance / currentGoal;
+
+      final width = constraints.maxWidth;
+      final proportionedWidth =
+          width - width * 0.25 * ((previousGoal != 0) ? 2 : 1);
+      final height = 20.0;
 
       return Stack(
         children: [
-
           Container(
-            width: width, height: height,
+            width: width,
+            height: height,
             decoration: BoxDecoration(
                 color: Colors.grey,
-                borderRadius: BorderRadius.all(Radius.circular(12))
+                borderRadius: BorderRadius.all(Radius.circular(12))),
+          ),
+          Positioned(
+            child: Container(
+              width: proportionedWidth * currentToCurrentGoal +
+                  ((previousGoal == 0) ? 0 : width * 0.25),
+              height: height,
+              decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.all(Radius.circular(12))),
             ),
           ),
-          Positioned(child: Container(
-            width: currentWith*currentToCurrentGoal, height: height,
-            decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.all(Radius.circular(12))
-            ),
-          ),),
           if (previousGoal > 0)
-            Positioned(child: Container(
-              width: 6,
-              height: height,
-              color: Colors.blue,
-            )
-              ,top: 0, left: (currentGoal == -1) ? currentWith * (previousGoal / balance) : width * 0.25,),
+            Positioned(
+              child: Container(
+                width: 6,
+                height: height,
+                color: Colors.blue,
+              ),
+              top: 0,
+              left: (currentGoal == -1)
+                  ? proportionedWidth * (previousGoal / balance)
+                  : width * 0.25,
+            ),
           if (currentGoal > 0)
-          Positioned(child: Container(
-            width: 6,
-            height: height,
-            color: Colors.blue,
-          )
-            ,top: 0, right: width * 0.25,),
+            Positioned(
+              child: Container(
+                width: 6,
+                height: height,
+                color: Colors.blue,
+              ),
+              top: 0,
+              right: width * 0.25,
+            ),
+          // TODO Show next goal mark
           // Add next goal mark
         ],
       );
     });
-
   }
 }
